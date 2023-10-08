5fa501c20726e33f07385491da4934e5f74bcb95
Emmanuel Oladiran <oladirane@gmail.com>
2023-10-08 22:39:37 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+// const ola = document.querySelector(".plus");
+// const oladiran = document.querySelector(".men");
+// const emmy = document.querySelectorAll(".men li");
+
+// ola.addEventListener("click", () => {
+//   oladiran.classList.toggle("active");
+// });
+
+// emmy.forEach((item) =>
+//   item.addEventListener("click", () => {
+//     oladiran.classList.toggle("active");
+//   })
+// );
